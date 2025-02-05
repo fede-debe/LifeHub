bb8e1c086ba6e2ea2d1fe66e3336005535c0c7b2
fede-debe <federico.debenedictis33@gmail.com>
2025-02-05 21:54:38 +0100

    feat: Implement Navigation and add NavGraph
    - Adds `Navigation` with `Destinations`, `Screens` and `NavigationActions`.
    - Creates `NavGraph` to manage app navigation.
    - Adds `HomeScreen` composable in `NavGraph`.
    - Updates `MainActivity` to use `NavGraph`.
    - Removes `Greeting` and `GreetingPreview` from `MainActivity`.

--- app/src/main/java/com/example/lifehub/navigation/NavGraph.kt
+++ app/src/main/java/com/example/lifehub/navigation/NavGraph.kt
@@ -1,2 +1,41 @@
 package com.example.lifehub.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.lifehub.ui.HomeScreen
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    startDestination: String = Destinations.HOME_ROUTE,
+    navActions: NavigationActions = remember(navController) {
+        NavigationActions(navController)
+    }
+) {
+    val currentNavBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = currentNavBackStackEntry?.destination?.route ?: startDestination
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(
+            Destinations.HOME_ROUTE
+        ) {
+           HomeScreen()
+        }
+    }
+
+}
--- app/src/main/java/com/example/lifehub/navigation/Navigation.kt
+++ app/src/main/java/com/example/lifehub/navigation/Navigation.kt
@@ -1,2 +1,22 @@
 package com.example.lifehub.navigation
 
+import androidx.navigation.NavHostController
+import com.example.lifehub.navigation.Screens.HOME_SCREEN
+
+/**
+ * Screens used in [Destinations]
+ */
+private object Screens {
+    const val HOME_SCREEN = "home"
+}
+
+/**
+ * Destinations used in the MainActivity
+ */
+object Destinations {
+    const val HOME_ROUTE = HOME_SCREEN
+}
+
+class NavigationActions(private val navController: NavHostController) {
+
+}
