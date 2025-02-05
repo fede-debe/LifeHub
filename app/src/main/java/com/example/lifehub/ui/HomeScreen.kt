c450b747b27c3c3d41de8b767e74e9a866a91248
fede-debe <federico.debenedictis33@gmail.com>
2025-02-05 21:55:05 +0100

    feat: Implement Home Screen with UiStateScreenContainer and Empty State
    - Adds `HomeScreen` composable that utilizes a `Scaffold` for basic layout.
    - Implements `HomeScreenContent` to display content within the home screen.
    - Introduces `UiStateScreenContainer` in `ScreenUtils.kt` to handle loading, empty, and valid content states with pull-to-refresh functionality.
    - Creates `EmptyState` composable to display when there's no content.
    - Adds a placeholder for content with repeated "Title category" texts.
    - Adds a placeholder for content in `emptyContent` of `UiStateScreenContainer`.
    - Adds `EmptyState` and `UiStateScreenContainer` in `ScreenUtils.kt`.

--- app/src/main/java/com/example/lifehub/ui/HomeScreen.kt
+++ app/src/main/java/com/example/lifehub/ui/HomeScreen.kt
@@ -1,2 +1,54 @@
 package com.example.lifehub.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.lifehub.util.UiStateScreenContainer
+
+@Composable
+fun HomeScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize(),) { innerPadding ->
+        HomeScreenContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+
+@Composable
+private fun HomeScreenContent(modifier: Modifier) {
+    UiStateScreenContainer(
+        loading = false,
+        modifier = modifier,
+        empty = false,
+        emptyContent = {
+            // todo content
+        },
+        onRefresh = {}
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    horizontal = 16.dp
+                )
+        ) {
+            repeat(4) {
+                Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Text(text = "Title category")
+//                    EmptyState(iconRes = R.drawable.baseline_attractions_24) {
+//                        // todo onclick
+//                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lifehub/util/ScreenUtils.kt
@@ -0,0 +1,74 @@
+package com.example.lifehub.util
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+/**
+ * A reusable container composable that handles loading, empty, and valid content states
+ * with pull-to-refresh support.
+ *
+ * @param loading When true, indicates that content is loading.
+ * @param empty When true, displays the [emptyContent] slot.
+ * @param emptyContent The composable to display when there's no content.
+ * @param onRefresh The callback triggered when the user pulls to refresh.
+ * @param modifier Modifier to be applied to the container.
+ * @param content The main content to display when not empty.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UiStateScreenContainer(
+    loading: Boolean,
+    empty: Boolean,
+    emptyContent: @Composable () -> Unit,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val pullRefreshState = rememberPullToRefreshState()
+
+    PullToRefreshBox(
+        state = pullRefreshState,
+        modifier = modifier,
+        isRefreshing = loading,
+        onRefresh = onRefresh
+    ) {
+        if (empty) {
+            emptyContent()
+        } else {
+            content()
+        }
+    }
+}
+
+
+@Composable
+fun EmptyState(
+    @DrawableRes iconRes: Int,
+    onClick: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Icon(
+            painter = painterResource(iconRes),
+            tint = Color.Black,
+            contentDescription = "empty state icon"
+        )
+        Text(text = "!!! Info text todo !!!", textAlign = TextAlign.Center)
+        Button(onClick = onClick) {
+            Text(text = "Button", textAlign = TextAlign.Center)
+        }
+    }
+}
