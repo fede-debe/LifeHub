383dcba4f3b100d5cb16092c230d48bdad04df2f
fede-debe <federico.debenedictis33@gmail.com>
2025-02-07 14:39:57 +0100

    feat: Add new components: Button and Inputs
    - Adds new `Button` component with primary, outlined, and text variants.
    - Adds `TextInput` component with support for error messages, character limits, and optional labels.
    - Adds `SupportingTextInput` to display error messages and character counters.
    - Adds `InputLabel` component to label form inputs with optional "optional" indicator.
    - Adds `InputInfoText` component to display information text for form inputs.

---
+++ app/src/main/java/com/example/lifehub/ui/components/Button.kt
@@ -0,0 +1,135 @@
+package com.example.lifehub.ui.components
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lifehub.theme.LifeHubTheme
+
+/**
+ * A primary button styled using Material 3's [Button]. This button is used for key actions
+ * that require the user's attention, such as submitting forms or proceeding to the next step.
+ *
+ * @param onClick The callback triggered when the button is clicked.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Whether the button is enabled or disabled.
+ * @param interactionSource Tracks user interactions such as clicks or hovers.
+ * @param contentPadding Padding to apply inside the button.
+ * @param content The composable content inside the button.
+ */
+@Composable
+fun ButtonPrimary(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    contentPadding: PaddingValues = PaddingValues(horizontal = 26.dp, vertical = 10.dp),
+    content: @Composable RowScope.() -> Unit
+) = Button(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    interactionSource = interactionSource,
+    content = content,
+    shape = LifeHubTheme.shapes.medium,
+    contentPadding = contentPadding
+)
+
+@Preview(showBackground = true, backgroundColor = 0xD1D5DB)
+@Composable
+fun PreviewButtonPrimary() {
+    LifeHubTheme {
+        ButtonPrimary(onClick = {}) {
+            Text("Primary Button")
+        }
+    }
+}
+
+/**
+ * An outlined button styled using Material 3's [OutlinedButton]. This button is used for secondary actions
+ * that support the main workflow but don’t require primary emphasis.
+ *
+ * @param onClick The callback triggered when the button is clicked.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Whether the button is enabled or disabled.
+ * @param interactionSource Tracks user interactions such as clicks or hovers.
+ * @param contentPadding Padding to apply inside the button.
+ * @param content The composable content inside the button.
+ */
+@Composable
+fun ButtonOutlined(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    contentPadding: PaddingValues = PaddingValues(horizontal = 26.dp, vertical = 10.dp),
+    content: @Composable RowScope.() -> Unit
+) = OutlinedButton(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    interactionSource = interactionSource,
+    content = content,
+    shape = LifeHubTheme.shapes.medium,
+    contentPadding = contentPadding,
+    colors = ButtonDefaults.outlinedButtonColors(containerColor = MaterialTheme.colorScheme.surface)
+)
+
+@Preview(showBackground = true, backgroundColor = 0xD1D5DB)
+@Composable
+fun PreviewButtonOutlined() {
+    LifeHubTheme {
+        ButtonOutlined(onClick = {}) {
+            Text("Outlined Button")
+        }
+    }
+}
+
+/**
+ * A text button styled using Material 3's [TextButton]. This button is used for low-priority or less intrusive
+ * actions that don’t require a strong emphasis, such as "Cancel" or "Learn More".
+ *
+ * @param onClick The callback triggered when the button is clicked.
+ * @param modifier Modifier to be applied to the button.
+ * @param enabled Whether the button is enabled or disabled.
+ * @param interactionSource Tracks user interactions such as clicks or hovers.
+ * @param contentPadding Padding to apply inside the button.
+ * @param content The composable content inside the button.
+ */
+@Composable
+fun ButtonText(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    contentPadding: PaddingValues = PaddingValues(horizontal = 26.dp, vertical = 10.dp),
+    content: @Composable RowScope.() -> Unit
+) = TextButton(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    interactionSource = interactionSource,
+    content = content,
+    shape = LifeHubTheme.shapes.medium,
+    contentPadding = contentPadding
+)
+
+@Preview(showBackground = true, backgroundColor = 0xD1D5DB)
+@Composable
+fun PreviewButtonText() {
+    LifeHubTheme {
+        ButtonText(onClick = {}) {
+            Text("Text Button")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lifehub/ui/components/Inputs.kt
@@ -0,0 +1,250 @@
+package com.example.lifehub.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lifehub.theme.LifeHubTheme
+
+@Composable
+fun TextInput(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    inputModifier: Modifier = Modifier,
+    errorText: String? = null,
+    maxCharacters: Int? = null,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    inputTitle: String? = null,
+    label: @Composable (() -> Unit)? = { InputLabel(inputTitle ?: "") },
+    isOptional: Boolean = false,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = {
+        SupportingTextInput(
+            errorText,
+            maxCharacters,
+            value
+        )
+    },
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = true,
+    maxLines: Int = 1
+) {
+    Column(modifier = modifier) {
+        if (inputTitle != null) {
+            InputLabel(text = inputTitle, isOptional = isOptional)
+        }
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = {
+                if (it.length <= (maxCharacters ?: Int.MAX_VALUE)) onValueChange(it)
+            },
+            isError = errorText != null,
+            label = label,
+            placeholder = placeholder,
+            modifier = inputModifier
+                .fillMaxWidth(),
+            enabled = enabled,
+            readOnly = readOnly,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            supportingText = supportingText,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            shape = MaterialTheme.shapes.small,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedPlaceholderColor = LifeHubTheme.colors.textColorSecondary,
+                focusedPlaceholderColor = MaterialTheme.colorScheme.secondary,
+                unfocusedTextColor = MaterialTheme.colorScheme.onSurface,
+                focusedTextColor = MaterialTheme.colorScheme.onSurface,
+                cursorColor = MaterialTheme.colorScheme.secondary,
+                focusedBorderColor = MaterialTheme.colorScheme.secondary,
+                unfocusedContainerColor = Color(0XFFE1F5FE),
+                focusedContainerColor = Color(0XFFE1F5FE)
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextInputPreview() {
+    LifeHubTheme {
+        var text by remember { mutableStateOf("") }
+        TextInput(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text(text = "Deposit Amount") },
+            placeholder = { Text(text = "Balance: 14.559 Sushi") },
+            errorText = null
+        )
+    }
+}
+
+/**
+ * A composable that displays supporting text for an input field, such as error messages
+ * or character counters.
+ *
+ * - If an error is present, it displays the error message on the left.
+ * - If a character limit is set, it displays the character counter on the right.
+ *
+ * @param errorText The error message to display, or null if there’s no error.
+ * @param maxCharacters The maximum allowed character count, or null if unlimited.
+ * @param value The current input value, used to calculate the character count.
+ */
+@Composable
+private fun SupportingTextInput(errorText: String?, maxCharacters: Int?, value: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = LifeHubTheme.spacing.stack.extraSmall),
+        horizontalArrangement = if (errorText == null) Arrangement.End else Arrangement.SpaceBetween
+    ) {
+        if (errorText != null) {
+            Text(
+                text = errorText,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+        if (maxCharacters != null) {
+            Text(
+                text = "${value.length} / $maxCharacters",
+                style = MaterialTheme.typography.bodySmall,
+                color = LifeHubTheme.colors.textColorSecondary,
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSupportingTextInputWithError() {
+    LifeHubTheme {
+        SupportingTextInput(
+            errorText = "Invalid input",
+            maxCharacters = 50,
+            value = "Current input"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSupportingTextInputWithoutError() {
+    LifeHubTheme {
+        SupportingTextInput(
+            errorText = null,
+            maxCharacters = 50,
+            value = "Current input"
+        )
+    }
+}
+
+/**
+ * A label for a form input using Material 3 styling.
+ * @param text The text of the label.
+ * @param isOptional If true, the label will have an appended "optional" text after it.
+ */
+@Composable
+fun InputLabel(
+    text: String,
+    modifier: Modifier = Modifier,
+    isOptional: Boolean = false
+) {
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = LifeHubTheme.spacing.stack.extraSmall),
+        text = buildAnnotatedString {
+            withStyle(
+                style = MaterialTheme.typography.bodySmall.toSpanStyle().merge(
+                    SpanStyle(
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                )
+            ) {
+                append(text)
+            }
+            if (isOptional) {
+                withStyle(
+                    style = MaterialTheme.typography.labelSmall.toSpanStyle().copy(
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                ) {
+                    append(" - ")
+                    append("Optional")
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InputLabelPreview() {
+    LifeHubTheme {
+        InputLabel(text = "Username", isOptional = true)
+    }
+}
+
+/**
+ * Styled text component to use as "information" text for a form input using Material 3 styling.
+ * @param text The information text to be displayed.
+ */
+@Composable
+fun InputInfoText(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodySmall,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = LifeHubTheme.spacing.stack.extraSmall)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InputInfoTextPreview() {
+    LifeHubTheme {
+        InputInfoText(text = "Your username must be at least 8 characters long.")
+    }
+}
+
+
