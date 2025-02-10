4c28a4f873a14a00a09ece9dbb2725d759c290ae
fede-debe <federico.debenedictis33@gmail.com>
2025-02-10 20:03:52 +0100

    feat: Add navigation to note details screen and update navigation routes
    - Adds a new `NoteDetailsScreen` to the navigation graph.
    - Updates `Navigation` class to include routes and arguments for note details.
    - Updates `NotesScreen` to navigate to `NoteDetailsScreen` when a note is clicked.
    - Updates `NotesScreen` to show SnackBar with message.
    - Updates `Navigation` class to receive message in `navigateToNotesScreen`.
    - Updates `AddNoteScreen` to navigate back using `navigateUp`.
    - Adds `DELETE_RESULT_OK` to signal delete action.
    - Adds `SNACK_BAR_MESSAGE_ARG` and `NOTE_ID` as arguments in `DestinationsArgs`.

--- app/src/main/java/com/example/lifehub/navigation/NavGraph.kt
+++ app/src/main/java/com/example/lifehub/navigation/NavGraph.kt
@@ -1,19 +1,26 @@
 package com.example.lifehub.navigation
 
+import android.app.Activity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.lifehub.navigation.DestinationsArgs.SNACK_BAR_MESSAGE_ARG
 import com.example.lifehub.ui.screens.home.HomeScreen
 import com.example.lifehub.ui.screens.notes.NotesScreen
 import com.example.lifehub.ui.screens.notes.add.AddNoteScreen
+import com.example.lifehub.ui.screens.notes.notedetails.NoteDetailsScreen
 import kotlinx.coroutines.CoroutineScope
+
+const val DELETE_RESULT_OK = Activity.RESULT_CANCELED + 1
 
 @Composable
 fun NavGraph(
@@ -39,13 +46,28 @@ fun NavGraph(
             HomeScreen(onClickCategory = { navActions.navigateToNotesScreen() })
         }
 
-        composable(Destinations.NOTES_ROUTE) {
-            NotesScreen(onClickAddNote = { navActions.navigateToAddNoteScreen()})
+        composable(Destinations.NOTES_ROUTE, arguments = listOf(
+            navArgument(SNACK_BAR_MESSAGE_ARG) { type = NavType.IntType; defaultValue = 0 }
+        )) { entry ->
+            NotesScreen(
+                snackBarMessage = entry.arguments?.getInt(SNACK_BAR_MESSAGE_ARG)!!,
+                onSnackBarMessageDisplayed = { entry.arguments?.putInt(SNACK_BAR_MESSAGE_ARG, 0) },
+                onClickAddNote = { navActions.navigateToAddNoteScreen() },
+                onNoteClick = { noteId ->
+                    navActions.navigateToNoteDetailScreen(noteId = noteId)
+                },
+                onClickBack = { navController.navigateUp() })
         }
 
         composable(Destinations.ADD_NOTE_ROUTE) {
-            AddNoteScreen(onBack = { navController.popBackStack()})
+            AddNoteScreen(onClickBack = { navController.navigateUp() })
+        }
+
+        composable(Destinations.NOTE_DETAILS_ROUTE) {
+            NoteDetailsScreen(onClickBack = { navController.navigateUp() },
+                onDeleteNote = {
+                    navActions.navigateToNotesScreen(DELETE_RESULT_OK)
+                })
         }
     }
-
 }
