a4c73d320d65ec962d32f1d47e4958d871b1f730
fede-debe <federico.debenedictis33@gmail.com>
2025-02-23 18:54:17 +0100

    refactor: Update BottomSheet component to use composable functions for content
    - Modifies `BottomSheet` to accept composable functions for title, description, and confirm button text.
    - Removes `title`, `description`, and `confirmButtonText` parameters in favor of composable lambda parameters.
    - Updates the content of `BottomSheet` to call the provided composable functions.

--- app/src/main/java/com/example/lifehub/ui/components/BottomSheet.kt
+++ app/src/main/java/com/example/lifehub/ui/components/BottomSheet.kt
@@ -2,25 +2,23 @@ package com.example.lifehub.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import com.example.lifehub.theme.LifeHubTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheet(
     sheetState: SheetState,
-    title: String,
-    description: String,
-    confirmButtonText: String,
+    title: @Composable ColumnScope.() -> Unit,
+    description: @Composable ColumnScope.() -> Unit,
+    confirmButtonText: @Composable () -> Unit,
     onDismiss: () -> Unit,
     onConfirm: () -> Unit
 ) {
@@ -39,21 +37,14 @@ fun BottomSheet(
                 modifier = Modifier.padding(top = LifeHubTheme.spacing.inset.extraSmall),
                 verticalArrangement = Arrangement.spacedBy(LifeHubTheme.spacing.stack.extraSmall)
             ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.headlineSmall,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Text(
-                    text = description,
-                    style = MaterialTheme.typography.bodyLarge
-                )
+                title()
+                description()
             }
             ButtonPrimary(
                 modifier = Modifier.fillMaxWidth(),
                 onClick = onConfirm
             ) {
-                Text(text = confirmButtonText)
+                confirmButtonText()
             }
         }
     }
